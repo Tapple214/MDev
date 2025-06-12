d222be0d659c390230bbc1e50776d200eaa840bd
Tapple214 <applegate21441@gmail.com>
2025-06-12 15:51:18 +0800

    name badge update

--- test/components/name-badge.js
+++ test/components/name-badge.js
@@ -1,26 +1,40 @@
 import * as ScreenOrientation from "expo-screen-orientation";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, SafeAreaView } from "react-native";
 
 export default function NameBadge({ name }) {
   ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_LEFT);
 
   return (
-    <>
-      <Text style={styles.welcomeText}>Hello</Text>
-      <Text style={styles.nameText}>{name}</Text>
-    </>
+    <View style={styles.container}>
+      <SafeAreaView style={styles.safeArea}>
+        <Text style={styles.welcomeText}>Hello</Text>
+        <Text style={styles.subtitleText}>my name is</Text>
+        <View style={styles.nameBox}>
+          <Text style={styles.nameText}>{name}</Text>
+        </View>
+      </SafeAreaView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  safeArea: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
   welcomeText: {
     fontSize: 90,
     textTransform: "uppercase",
     fontWeight: "bold",
     color: "white",
     textAlign: "center",
-    letterSpacing: 2,
-    lineHeight: 100,
   },
   subtitleText: {
     fontSize: 30,
@@ -29,8 +43,6 @@ const styles = StyleSheet.create({
     color: "white",
     marginBottom: 20,
     textAlign: "center",
-    letterSpacing: 1,
-    lineHeight: 35,
   },
   nameBox: {
     width: "100%",
