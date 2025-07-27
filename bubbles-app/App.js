0f300d196ceca8620bab96e30ed51667e2782c7f
Tapple214 <applegate21441@gmail.com>
2025-07-27 20:32:15 +0800

    named base pages

--- bubbles-app/App.js
+++ bubbles-app/App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import {
+  View,
+  ScrollView,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
+
+
+export default function BubblesApp() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Sreen name="Home" component={Home} />
+        <Stack.Sreen name="BubbleDetails" component={BubbleDetails} />
+        <Stack.Sreen name="BubbleBook" component={BubbleBook} />
+        <Stack.Sreen name="BubbleBudies" component={BubbleBudies} />
+        <Stack.Sreen name="CreateBubble" component={CreateBubble} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
