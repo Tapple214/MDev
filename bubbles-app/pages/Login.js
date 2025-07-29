71ad28dffdd60f2ac48d2aa74512416571c5044a
Tapple214 <applegate21441@gmail.com>
2025-07-29 17:40:10 +0800

    auth implementation

--- bubbles-app/pages/Login.js
+++ bubbles-app/pages/Login.js
@@ -1,14 +1,58 @@
-import React from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import React, { useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import * as Google from "expo-auth-session/providers/google";
+import { signInWithCredential, GoogleAuthProvider } from "firebase/auth";
+import { auth, db } from "../firebase";
+import { doc, setDoc } from "firebase/firestore";
 
 export default function Login({ navigation }) {
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId:
+      "1039980441242-4p6g2rvp1e9p3p6p6r1v7v7v7v7v7v7v.apps.googleusercontent.com",
+    iosClientId:
+      "603581775295-851ag1nphqt4tov1pofg0e9u1md2kh1t.apps.googleusercontent.com",
+    androidClientId:
+      "603581775295-3jeo8mvvah7p80acv86u65j5fi0avsiv.apps.googleusercontent.com",
+    webClientId:
+      "603581775295-rtaoqr6irikf29ca5rjbnm23ili9lgqg.apps.googleusercontent.com",
+  });
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      const { id_token } = response.params;
+      const credential = GoogleAuthProvider.credential(id_token);
+
+      signInWithCredential(auth, credential)
+        .then(async (userCredential) => {
+          const user = userCredential.user;
+          // Save user info to Firestore
+          await setDoc(doc(db, "users", user.uid), {
+            name: user.displayName,
+            email: user.email,
+          });
+          navigation.navigate("Home");
+        })
+        .catch((error) => {
+          Alert.alert("Login error", error.message);
+        });
+    }
+  }, [response]);
+
   return (
     <SafeAreaView style={styles.generalContainer}>
       <Image source={require("../assets/login.jpeg")} style={styles.image} />
       <TouchableOpacity
         style={styles.button}
-        onPress={() => navigation.navigate("Home")}
+        onPress={() => promptAsync()}
+        disabled={!request}
       >
         <Text>Sign in with Google</Text>
       </TouchableOpacity>
