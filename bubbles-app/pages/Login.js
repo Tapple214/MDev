80d743c12839add981e6e4b54602f3ab1f71bf5b
Tapple214 <applegate21441@gmail.com>
2025-07-29 20:29:55 +0800

    auth

--- bubbles-app/pages/Login.js
+++ bubbles-app/pages/Login.js
@@ -11,19 +11,35 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import * as Google from "expo-auth-session/providers/google";
 import { signInWithCredential, GoogleAuthProvider } from "firebase/auth";
 import { auth, db } from "../firebase";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import * as AuthSession from "expo-auth-session";
 
 export default function Login({ navigation }) {
+  // const redirectUri = AuthSession.makeRedirectUri({
+  //   // native: "bubbles://redirect",
+  //   useProxy: true,
+  // });
+
+  // const redirectUri = "https://auth.expo.io/@tapple/bubbles-app";
+
+  const redirectUri = AuthSession.makeRedirectUri({ useProxy: true });
+
+  console.log("hi");
+  console.log("Redirect URI:", redirectUri);
+
   const [request, response, promptAsync] = Google.useAuthRequest({
     expoClientId:
-      "1039980441242-4p6g2rvp1e9p3p6p6r1v7v7v7v7v7v7v.apps.googleusercontent.com",
+      "355374652560-bv8a0b9eh4joa9oo32lefa4ci403tkdp.apps.googleusercontent.com",
     iosClientId:
-      "603581775295-851ag1nphqt4tov1pofg0e9u1md2kh1t.apps.googleusercontent.com",
+      "355374652560-7074thqjtjf55j2h1050hvq0n3a4tohu.apps.googleusercontent.com",
     androidClientId:
-      "603581775295-3jeo8mvvah7p80acv86u65j5fi0avsiv.apps.googleusercontent.com",
+      "355374652560-f1cah0s5bc6f9cb9i4d543hdnjril21u.apps.googleusercontent.com",
     webClientId:
-      "603581775295-rtaoqr6irikf29ca5rjbnm23ili9lgqg.apps.googleusercontent.com",
+      "355374652560-bv8a0b9eh4joa9oo32lefa4ci403tkdp.apps.googleusercontent.com",
+    redirectUri,
   });
+
+  console.log(redirectUri);
 
   useEffect(() => {
     if (response?.type === "success") {
@@ -33,12 +49,20 @@ export default function Login({ navigation }) {
       signInWithCredential(auth, credential)
         .then(async (userCredential) => {
           const user = userCredential.user;
-          // Save user info to Firestore
-          await setDoc(doc(db, "users", user.uid), {
-            name: user.displayName,
-            email: user.email,
-          });
-          navigation.navigate("Home");
+          const userDocRef = doc(db, "users", user.uid);
+
+          const userDoc = await getDoc(userDocRef);
+
+          if (userDoc.exists()) {
+            navigation.navigate("Home");
+          } else {
+            // Save user info to Firestore
+            await setDoc(doc(db, "users", user.uid), {
+              name: user.displayName,
+              email: user.email,
+            });
+            navigation.navigate("Home");
+          }
         })
         .catch((error) => {
           Alert.alert("Login error", error.message);
@@ -51,8 +75,8 @@ export default function Login({ navigation }) {
       <Image source={require("../assets/login.jpeg")} style={styles.image} />
       <TouchableOpacity
         style={styles.button}
-        // onPress={() => promptAsync()}
-        onPress={() => navigation.navigate("Home")}
+        onPress={() => promptAsync()}
+        // onPress={() => navigation.navigate("Home")}
         disabled={!request}
       >
         <Text>Sign in with Google</Text>
