a5cba16cb982a87ec745f3320f7b29aa764fc1fa
Tapple214 <applegate21441@gmail.com>
2025-07-30 20:18:58 +0800

    Update BubbleBuddies.js

--- bubbles-app/pages/BubbleBuddies.js
+++ bubbles-app/pages/BubbleBuddies.js
@@ -1,15 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import NavBar from "../components/navbar";
+import { useAuth } from "../contexts/AuthContext";
+import { getUser } from "../utils/firestore";
 
 export default function BubbleBuddies() {
+  const { user } = useAuth();
+  const [userData, setUserData] = useState(null);
+  // const [bubbleBuddies, setBubbleBuddies] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      if (user && user.uid) {
+        const fetchedUserData = await getUser(user.uid);
+        console.log("User data from Firestore:", fetchedUserData);
+        setUserData(fetchedUserData);
+      }
+    };
+    fetchUserData();
+  }, [user]);
+
+  console.log("bubble buddies", userData?.bubbleBuddies[0]);
+
   return (
     <View style={styles.generalContainer}>
-      <NavBar />
       <Text style={styles.subTitle}>
         A list of your go-to invitees ready for your next bubble!{" "}
       </Text>
+
+      <View style={styles.bubbleBuddiesContainer}>
+        {userData?.bubbleBuddies && userData.bubbleBuddies.length > 0 ? (
+          userData.bubbleBuddies.map((buddy, index) => (
+            <View style={styles.bubbleBuddyNameContainer}>
+              <Text key={index}>{buddy}</Text>
+            </View>
+          ))
+        ) : (
+          <Text style={styles.noBuddiesText}>No bubble buddies found</Text>
+        )}
+      </View>
+
+      <NavBar />
     </View>
   );
 }
@@ -26,12 +58,26 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     paddingBottom: 15,
   },
-  subTitle: { paddingHorizontal: 15, paddingBottom: 10 },
+  subTitle: { paddingHorizontal: 15 },
   input: {
     borderRadius: 5,
     padding: 10,
     marginHorizontal: 15,
     marginBottom: 15,
     backgroundColor: "#FEFADF",
+  },
+  bubbleBuddiesContainer: { paddingVertical: 15, paddingHorizontal: 15 },
+  bubbleBuddyNameContainer: {
+    backgroundColor: "#FEFADF",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    marginBottom: 15,
+  },
+  noBuddiesText: {
+    textAlign: "center",
+    fontSize: 16,
+    color: "#452A17",
+    marginTop: 20,
   },
 });
